4a417ab45eae0cfdcdc7498415e379ac4953d875
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-19 16:53:26 -0300

    Initial Commit

---
+++ TwitterSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Samuel Brehm on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Samuel Brehm on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
