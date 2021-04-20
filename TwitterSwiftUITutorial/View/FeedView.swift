490ec8a48918623ac16dd2ecdfc3a3a2dbc94b93
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-19 22:48:15 -0300

    Create float button of feed main

--- TwitterSwiftUITutorial/View/FeedView.swift
+++ TwitterSwiftUITutorial/View/FeedView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                
+            }
+            
+            Button(action: {}, label: {
+                Image("tweet")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
             
         }
     }
